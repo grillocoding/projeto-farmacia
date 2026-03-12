285509e6c324b83072701efe1d0288b4103a7ada
Lucas Grillo <lucasgrillosaldanha@protonmail.com>
2026-03-12 20:34:56 -0300

    add new features

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -60,12 +60,7 @@
                     <input type="text" name="phone" value="{{ old('phone') }}" placeholder="(00) 00000-0000"
                         class="w-full border border-gray-300 rounded px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-teal-400">
                 </div>
-
-                <div>
-                    <label class="block text-sm font-medium text-gray-700 mb-1">Endereço</label>
-                    <input type="text" name="address" value="{{ old('address') }}"
-                        class="w-full border border-gray-300 rounded px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-teal-400">
-                </div>{{-- CEP --}}
+                {{-- CEP --}}
                 <div>
                     <label class="block text-sm font-medium text-gray-700 mb-1">CEP</label>
                     <div class="relative">
